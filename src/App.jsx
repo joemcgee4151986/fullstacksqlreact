ed72d39667944d9d3c5a6aa0080b293bd4decc9b
joemcgee4151986 <joerobertmcgee@gmail.com>
2022-12-31 17:21:29 +0900

    fixing button in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
             value={name}
             onChange={e => setName(e.target.value)}
           />
-          <button className="btn btn-primary" id="submit">Submit</button>
+          <button  value=" Send" class="btn btn-primary" type="submit" id="submit">Submit </button>
         </form>
         <table className="table my-5">
           <thead>
